e0af6f2a4d223813fb773510846b558bbd6b1d73
michaelposada <michaelposada@mail.adelphi.edu>
2019-06-25 16:14:18 -0400

    Able to check path and create file, unable to record.

--- timelapseApp/src/NDPluginTimelapse.cpp
+++ timelapseApp/src/NDPluginTimelapse.cpp
@@ -1,22 +1,28 @@
 /**
  * This file is a basic template for implementing areaDetector plugins.
- * You must implement all of the functions already listed here along with any 
+ * You must implement all of the functions already listed here along with any
  * additional plugin specific functions you require.
- * 
- * Author:Michael Posada	
+ *
+ * Author:Michael Posada
  * Created on: 6/19/2019
- * 
+ *
  */
 
 
 
-//include some standard libraries
+ //include some standard libraries
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 #include <iostream>
 #include <stdio.h>
+#include <fstream>
+#include <filesystem>
+#include <experimental/filesystem> // C++-standard header file name
+#include <thread>
+#include <sys/stat.h>
 
+#include <libxml/parser.h>
 
 //include epics/area detector libraries
 #include <epicsMutex.h>
@@ -35,11 +41,11 @@ using namespace std;
 using namespace cv;
 
 // Name your plugin
-static const char *pluginName="NDPluginTimelapse";
+static const char* pluginName = "NDPluginTimelapse";
 
-asynStatus NDPluginTimelapse::writeOctet(asynUser* pasynUser, const char *value, size_t nChars, size_t *nActual)
+asynStatus NDPluginTimelapse::writeOctet(asynUser* pasynUser, const char* value, size_t nChars, size_t* nActual)
 {
-    printf("Inside writeOctet\n");
+	printf("Inside writeOctet\n");
 	const char* functionName = "writeOctet";
 	int function = pasynUser->reason;
 	asynStatus status = asynSuccess;
@@ -47,20 +53,16 @@ asynStatus NDPluginTimelapse::writeOctet(asynUser* pasynUser, const char *value,
 
 	status = setStringParam(function, value);
 
-    String check;
-    //getStringParam(NDPluginTimelapseTlFilename, &check);
-
-    //printf(check+"\n");
 
 	asynPrint(this->pasynUserSelf, ASYN_TRACEIO_DRIVER, "%s::%s function = %d value=%d\n", pluginName, functionName, function, value);
 	printf("Error after status inside Octet\n");
-    // replace PLUGINNAME with your plugin (ex. BAR)
-	if(function < ND_TIMELAPSE_FIRST_PARAM){
+	// replace PLUGINNAME with your plugin (ex. BAR)
+	if (function < ND_TIMELAPSE_FIRST_PARAM) {
 		status = NDPluginDriver::writeOctet(pasynUser, value, nChars, nActual);
 	}
 	printf("Right before the callBack, inside Octet\n");
 	callParamCallbacks();
-	if(status){
+	if (status) {
 		asynPrint(this->pasynUserSelf, ASYN_TRACE_ERROR, "%s::%s Error writing Octet val to PV\n", pluginName, functionName);
 	}
 	return status;
@@ -68,20 +70,20 @@ asynStatus NDPluginTimelapse::writeOctet(asynUser* pasynUser, const char *value,
 
 /**
  * Override of NDPluginDriver function. Must be implemented by your plugin
- * 
+ *
  * @params[in]: pasynUser	-> pointer to asyn User that initiated the transaction
  * @params[in]: value		-> value PV was set to
  * @return: success if PV was updated correctly, otherwise error
  */
 
 
-asynStatus NDPluginTimelapse::writeInt32(asynUser* pasynUser, epicsInt32 value){
+asynStatus NDPluginTimelapse::writeInt32(asynUser* pasynUser, epicsInt32 value) {
 	const char* functionName = "writeInt32";
 	int function = pasynUser->reason;
 	asynStatus status = asynSuccess;
 	int checkStatus;
 	getIntegerParam(NDPluginTimelapseTlRecord, &checkStatus);
-	if(checkStatus == 1 && value == 1)
+	if (checkStatus == 1 && value == 1)
 	{
 		//nothing happens
 	}
@@ -90,19 +92,19 @@ asynStatus NDPluginTimelapse::writeInt32(asynUser* pasynUser, epicsInt32 value){
 		//start recording
 		//setIntegerParam(NDPluginTimelapseTlRecord, 1);
 	}
-	else if(checkStatus == 1 && value ==0)
+	else if (checkStatus == 1 && value == 0)
 	{
 		//end recording
 		//setIntegerParam(NDPluginTimelapseTlRecord, 0);
 	}
 	status = setIntegerParam(function, value);
 	asynPrint(this->pasynUserSelf, ASYN_TRACEIO_DRIVER, "%s::%s function = %d value=%d\n", pluginName, functionName, function, value);
-    // replace PLUGINNAME with your plugin (ex. BAR)
-	if(function < ND_TIMELAPSE_FIRST_PARAM){
+	// replace PLUGINNAME with your plugin (ex. BAR)
+	if (function < ND_TIMELAPSE_FIRST_PARAM) {
 		status = NDPluginDriver::writeInt32(pasynUser, value);
 	}
 	callParamCallbacks();
-	if(status){
+	if (status) {
 		asynPrint(this->pasynUserSelf, ASYN_TRACE_ERROR, "%s::%s Error writing Int32 val to PV\n", pluginName, functionName);
 	}
 	return status;
@@ -116,9 +118,9 @@ asynStatus NDPluginTimelapse::writeInt32(asynUser* pasynUser, epicsInt32 value){
  * @params[in]: pArray -> NDArray recieved by the plugin from the camera
  * @return: void
 */
-void NDPluginTimelapse::processCallbacks(NDArray *pArray){
+void NDPluginTimelapse::processCallbacks(NDArray* pArray) {
 	static const char* functionName = "processCallbacks";
-	NDArray *pScratch;
+	NDArray* pScratch;
 	asynStatus status = asynSuccess;
 	NDArrayInfo arrayInfo;
 	//call base class and get information about frame
@@ -130,48 +132,51 @@ void NDPluginTimelapse::processCallbacks(NDArray *pArray){
 	this->unlock();
 
 	// Process your image here.
-    // Access data with pArray->pData
-    // DO NOT CALL pArray.release()
+	// Access data with pArray->pData
+	// DO NOT CALL pArray.release()
 	// If used, call pScratch.release()
-    // use doCallbacksGenericPointer with pScratch to pass processed image to plugin array port
+	// use doCallbacksGenericPointer with pScratch to pass processed image to plugin array port
 	int checkStatus;
+
 	getIntegerParam(NDPluginTimelapseTlRecord, &checkStatus);
-	//VideoCapture cap(0);
-	//if(checkStatus == 0)
-	//{
-		//printf("Recording is off!\n");
-		//VideoCapture cap(0);
-		//cap.release();
-		//end the recording call the library and stuff
-	
-    //}
-	//else if (checkStatus == 1)
-	//{
-	//	printf("Recording on!\n");
-		///start the recording call
-		
-		//Note to self we can add the file path name to save the reording somewhere
-		//when asking them for the information on
-		
-	//}
-	//else
-	//{
 
-	//}
+	char pathing[256];
 
-    /*
-	if(cap.isOpened() == true) 
+	(asynStatus)getStringParam(NDPluginTimelapseTlFilename, sizeof(pathing), pathing);
+
+	std::string finalPath = pathing;
+
+
+	std::cout << finalPath << "\n";
+	// change the if statement to do without filesystem
+	struct stat buffer;
+
+	FILE* path = fopen(pathing, "wb");
+
+	if (path != NULL)
 	{
-		printf("On!\n");
+		fclose(path);
+		//I want to call a thread to set up the recording process of the camera
+		//recording();
+		//thread first(recording);
+		//first.detach();
+		//first.join();
+		printf("Valid Pathing, we are good to record!\n");
+
+		VideoCapture cap(finalPath);
+		cap.open(finalPath);
+
+
+
 	}
-	else
-	{
-		printf("Off!\n");
+	else {
+		printf("Not valid");
 	}
-    */
+
+
 	this->lock();
 
-	if(status == asynError){
+	if (status == asynError) {
 		asynPrint(this->pasynUserSelf, ASYN_TRACE_ERROR, "%s::%s Error, image not processed correctly\n", pluginName, functionName);
 		return;
 	}
@@ -179,30 +184,45 @@ void NDPluginTimelapse::processCallbacks(NDArray *pArray){
 	callParamCallbacks();
 }
 
+//A seprate thread for video recording
 
+void NDPluginTimelapse::recording()
+{
+	char pathing;
+	string finalPath;
+	int addrs = 0;
+
+	(asynStatus)getStringParam(addrs, NDPluginTimelapseTlFilename, &pathing); //Not allowed to do this
+
+	finalPath = pathing + "";
+	VideoCapture cap(finalPath);
+
+
+
+}
 
 //constructror from base class, replace with your plugin name
-NDPluginTimelapse::NDPluginTimelapse(const char *portName, int queueSize, int blockingCallbacks,
-		const char *NDArrayPort, int NDArrayAddr,
-		int maxBuffers, size_t maxMemory,
-		int priority, int stackSize)
-		/* Invoke the base class constructor */
-		: NDPluginDriver(portName, queueSize, blockingCallbacks,
-        NDArrayPort, NDArrayAddr, 1, maxBuffers, maxMemory,
-        asynInt32ArrayMask | asynFloat64ArrayMask | asynGenericPointerMask,
-        asynInt32ArrayMask | asynFloat64ArrayMask | asynGenericPointerMask,
-        ASYN_MULTIDEVICE, 1, priority, stackSize, 1)
+NDPluginTimelapse::NDPluginTimelapse(const char* portName, int queueSize, int blockingCallbacks,
+	const char* NDArrayPort, int NDArrayAddr,
+	int maxBuffers, size_t maxMemory,
+	int priority, int stackSize)
+	/* Invoke the base class constructor */
+	: NDPluginDriver(portName, queueSize, blockingCallbacks,
+		NDArrayPort, NDArrayAddr, 1, maxBuffers, maxMemory,
+		asynInt32ArrayMask | asynFloat64ArrayMask | asynGenericPointerMask,
+		asynInt32ArrayMask | asynFloat64ArrayMask | asynGenericPointerMask,
+		ASYN_MULTIDEVICE, 1, priority, stackSize, 1)
 {
-	createParam(NDPluginTimelapseTlFilenameString, 	asynParamOctet, 	&NDPluginTimelapseTlFilename);
-	createParam(NDPluginTimelapseTlRecordString, 	asynParamInt32, 	&NDPluginTimelapseTlRecord);
+	createParam(NDPluginTimelapseTlFilenameString, asynParamOctet, &NDPluginTimelapseTlFilename);
+	createParam(NDPluginTimelapseTlRecordString, asynParamInt32, &NDPluginTimelapseTlRecord);
 
 	char versionString[25];
-	
+
 	// Create PV parameters here
-    // EXAMPLE:
-    // createParam(PVString, 	asynParamOctet, 	&PVIndex);  -> string and waveform records
-    // createParam(PVString, 	asynParamInt32, 	&PVIndex);  -> int records
-    // createParam(PVString, 	asynParamFloat64, 	&PVIndex);  -> float records
+	// EXAMPLE:
+	// createParam(PVString, 	asynParamOctet, 	&PVIndex);  -> string and waveform records
+	// createParam(PVString, 	asynParamInt32, 	&PVIndex);  -> int records
+	// createParam(PVString, 	asynParamFloat64, 	&PVIndex);  -> float records
 
 	setStringParam(NDPluginDriverPluginType, "NDPluginTimelapse");
 	epicsSnprintf(versionString, sizeof(versionString), "%d.%d.%d", TIMELAPSE_VERSION, TIMELAPSE_REVISION, TIMELAPSE_MODIFICATION);
@@ -215,31 +235,31 @@ NDPluginTimelapse::NDPluginTimelapse(const char *portName, int queueSize, int bl
 /**
  * External configure function. This will be called from the IOC shell of the
  * detector the plugin is attached to, and will create an instance of the plugin and start it
- * 
+ *
  * @params[in]	-> all passed to constructor
  */
-extern "C" int NDTimelapseConfigure(const char *portName, int queueSize, int blockingCallbacks,
-		const char *NDArrayPort, int NDArrayAddr,
-		int maxBuffers, size_t maxMemory,
-		int priority, int stackSize){
+extern "C" int NDTimelapseConfigure(const char* portName, int queueSize, int blockingCallbacks,
+	const char* NDArrayPort, int NDArrayAddr,
+	int maxBuffers, size_t maxMemory,
+	int priority, int stackSize) {
 
-	NDPluginTimelapse *pPlugin = new NDPluginTimelapse(portName, queueSize, blockingCallbacks, NDArrayPort, NDArrayAddr,
+	NDPluginTimelapse* pPlugin = new NDPluginTimelapse(portName, queueSize, blockingCallbacks, NDArrayPort, NDArrayAddr,
 		maxBuffers, maxMemory, priority, stackSize);
 	return pPlugin->start();
 }
 
 
 /* IOC shell arguments passed to the plugin configure function */
-static const iocshArg initArg0 = { "portName",iocshArgString};
-static const iocshArg initArg1 = { "frame queue size",iocshArgInt};
-static const iocshArg initArg2 = { "blocking callbacks",iocshArgInt};
-static const iocshArg initArg3 = { "NDArrayPort",iocshArgString};
-static const iocshArg initArg4 = { "NDArrayAddr",iocshArgInt};
-static const iocshArg initArg5 = { "maxBuffers",iocshArgInt};
-static const iocshArg initArg6 = { "maxMemory",iocshArgInt};
-static const iocshArg initArg7 = { "priority",iocshArgInt};
-static const iocshArg initArg8 = { "stackSize",iocshArgInt};
-static const iocshArg * const initArgs[] = {&initArg0,
+static const iocshArg initArg0 = { "portName",iocshArgString };
+static const iocshArg initArg1 = { "frame queue size",iocshArgInt };
+static const iocshArg initArg2 = { "blocking callbacks",iocshArgInt };
+static const iocshArg initArg3 = { "NDArrayPort",iocshArgString };
+static const iocshArg initArg4 = { "NDArrayAddr",iocshArgInt };
+static const iocshArg initArg5 = { "maxBuffers",iocshArgInt };
+static const iocshArg initArg6 = { "maxMemory",iocshArgInt };
+static const iocshArg initArg7 = { "priority",iocshArgInt };
+static const iocshArg initArg8 = { "stackSize",iocshArgInt };
+static const iocshArg* const initArgs[] = { &initArg0,
 					&initArg1,
 					&initArg2,
 					&initArg3,
@@ -247,24 +267,24 @@ static const iocshArg * const initArgs[] = {&initArg0,
 					&initArg5,
 					&initArg6,
 					&initArg7,
-					&initArg8};
+					&initArg8 };
 
 
 // Define the path to your plugin's extern configure function above
-static const iocshFuncDef initFuncDef = {"NDTimelapseConfigure",9,initArgs};
+static const iocshFuncDef initFuncDef = { "NDTimelapseConfigure",9,initArgs };
 
 
 /* link the configure function with the passed args, and call it from the IOC shell */
-static void initCallFunc(const iocshArgBuf *args){
+static void initCallFunc(const iocshArgBuf* args) {
 	NDTimelapseConfigure(args[0].sval, args[1].ival, args[2].ival,
-			args[3].sval, args[4].ival, args[5].ival,
-			args[6].ival, args[7].ival, args[8].ival);
+		args[3].sval, args[4].ival, args[5].ival,
+		args[6].ival, args[7].ival, args[8].ival);
 }
 
 
 /* function to register the configure function in the IOC shell */
-extern "C" void NDTimelapseRegister(void){
-	iocshRegister(&initFuncDef,initCallFunc);
+extern "C" void NDTimelapseRegister(void) {
+	iocshRegister(&initFuncDef, initCallFunc);
 }
 
 
